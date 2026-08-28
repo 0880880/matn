0049e6c7817f079a6609bb2f994ba2cda596fb5b
0880880 <98263509+0880880@users.noreply.github.com>
2026-08-28 16:09:18 +0330

    add variable weight layout and help text

--- src/test/java/com/github/zeroeighteightzero/matn/FontViewer.java
+++ src/test/java/com/github/zeroeighteightzero/matn/FontViewer.java
@@ -30,13 +30,15 @@ public class FontViewer implements ApplicationListener, Lwjgl3WindowListener {
     SpriteBatch batch;
     OrthographicCamera camera;
     ScreenViewport viewport;
+    ScreenViewport guiViewport;
     SimpleGPUGlyphBatch gpuBatch;
 
     GlyphAtlas atlas;
 
     Typeface face;
-    Font font;
-    Layout layout;
+    Typeface prevFace;
+    Font[] fonts;
+    Layout[] layouts;
 
     int fontSize = 64;
     float targetZoom = 1;
@@ -55,12 +57,73 @@ public class FontViewer implements ApplicationListener, Lwjgl3WindowListener {
 
     String text = "The quick brown fox jumps over the lazy dog.";
 
+    Typeface guiFace;
+    Font guiFont;
+    Layout guiLayout;
+
     boolean useGPU = false;
 
-    private void updateLayouts() {
-        layout = new Layout(text, fontSize);
-        layout.setFont(font);
-        layout.markup();
+    private void disposeFonts() {
+        if (fonts == null) {
+            return;
+        }
+        for (Font font : fonts) {
+            font.dispose();
+        }
+    }
+
+    private void update() {
+
+        if (face.weight() != null) {
+            float weightStart = face.weight().min;
+            float weightEnd = face.weight().max;
+            int stops = 8;
+            if (prevFace != face) {
+                fonts = new Font[stops];
+            }
+            layouts = new Layout[stops];
+            for (int i = 0; i < stops; i++) {
+                float t = i / (float) (stops-1);
+                float w = MathUtils.lerp(weightStart, weightEnd, t);
+
+                if (prevFace != face) {
+                    Font font = new Font(face, atlas);
+                    if (face.opticalSize() != null) {
+                        font.opticalSize(face.opticalSize().max);
+                    }
+                    font.weight(w);
+                    font.applyVariation();
+
+                    fonts[i] = font;
+                }
+
+                Layout layout = new Layout(text, fontSize);
+                layout.setFont(fonts[i]);
+                layout.markup();
+
+                layouts[i] = layout;
+            }
+        } else {
+            if (prevFace != face) {
+                fonts = new Font[1];
+
+                Font font = new Font(face, atlas);
+                if (face.opticalSize() != null) {
+                    font.opticalSize(face.opticalSize().max);
+                }
+                font.applyVariation();
+
+                fonts[0] = font;
+            }
+
+            layouts = new Layout[1];
+            Layout layout = new Layout(text, fontSize);
+            layout.setFont(fonts[0]);
+            layout.markup();
+
+            layouts[0] = layout;
+        }
+        prevFace = face;
     }
 
     @Override
@@ -69,6 +132,7 @@ public class FontViewer implements ApplicationListener, Lwjgl3WindowListener {
         batch = new SpriteBatch();
         camera = new OrthographicCamera();
         viewport = new ScreenViewport(camera);
+        guiViewport = new ScreenViewport();
 
         atlas = new GlyphAtlas(512, Pixmap.Format.RGBA8888);
 
@@ -76,14 +140,14 @@ public class FontViewer implements ApplicationListener, Lwjgl3WindowListener {
 
         gpuBatch = new SimpleGPUGlyphBatch();
 
-        font = new Font(face, atlas);
-        if (face.opticalSize() != null) {
-            font.opticalSize(face.opticalSize().max);
-        }
-        font.weight(500);
-        font.applyVariation();
+        update();
 
-        updateLayouts();
+        guiFace = new Typeface("Inter/Inter-VariableFont_opsz,wght.ttf");
+        guiFont = new Font(guiFace, atlas);
+        guiFont.outlineWidth = 3;
+        guiLayout = new Layout("Drop a font to view.\nCtrl+G to toggle GPU mode", 24);
+        guiLayout.setFont(guiFont);
+        guiLayout.markup();
 
         Gdx.input.setInputProcessor(new InputProcessor() {
             @Override
@@ -109,6 +173,7 @@ public class FontViewer implements ApplicationListener, Lwjgl3WindowListener {
                 } else {
                     text += character;
                 }
+                update();
                 return true;
             }
 
@@ -186,6 +251,7 @@ public class FontViewer implements ApplicationListener, Lwjgl3WindowListener {
     @Override
     public void resize(int width, int height) {
         viewport.update(width, height, true);
+        guiViewport.update(width, height, true);
     }
 
     @Override
@@ -216,7 +282,12 @@ public class FontViewer implements ApplicationListener, Lwjgl3WindowListener {
             gpuBatch.setProjectionMatrix(camera.combined);
             gpuBatch.begin();
 
-            layout.drawGPU(gpuBatch, 0, 0);
+            float y = 0;
+
+            for (Layout l : layouts) {
+                l.drawGPU(gpuBatch, 0, -y);
+                y += l.height;
+            }
 
             gpuBatch.end();
         } else {
@@ -225,10 +296,23 @@ public class FontViewer implements ApplicationListener, Lwjgl3WindowListener {
             batch.begin();
             batch.setColor(Color.WHITE);
 
-            layout.draw(batch, 0, 0);
+            float y = 0;
+
+            for (Layout l : layouts) {
+                l.draw(batch, 0, -y);
+                y += l.height;
+            }
 
             batch.end();
         }
+
+        batch.setProjectionMatrix(guiViewport.getCamera().combined);
+        batch.begin();
+        batch.setColor(Color.WHITE);
+
+        guiLayout.draw(batch, 0, Gdx.graphics.getHeight()-guiFont.getLineHeight(guiLayout.fontSize));
+
+        batch.end();
 
     }
 
@@ -242,8 +326,10 @@ public class FontViewer implements ApplicationListener, Lwjgl3WindowListener {
 
     @Override
     public void dispose() {
-        font.dispose();
+        disposeFonts();
+        guiFont.dispose();
         atlas.dispose();
+        guiFace.dispose();
         face.dispose();
     }
 
@@ -285,11 +371,10 @@ public class FontViewer implements ApplicationListener, Lwjgl3WindowListener {
                     Typeface newFace = new Typeface(Gdx.files.absolute(f));
                     atlas.dispose();
                     atlas = new GlyphAtlas(512);
-                    font.dispose();
-                    font = new Font(newFace, atlas);
+                    disposeFonts();
                     face.dispose();
                     face = newFace;
-                    updateLayouts();
+                    update();
                     break;
                 } catch (Exception e) {
                     System.err.println("Failed to load font \"" + f + "\"");
